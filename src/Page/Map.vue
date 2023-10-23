2b5616514df48e55b36f9f0288561b9ce3f8b0f0
Vadim Shemyatin <vshemyatin@MacBook-Pro-Vadim.local>
2023-10-23 17:31:25 +0300

    add quality comment

--- src/Page/Map.vue
+++ src/Page/Map.vue
@@ -31,6 +31,7 @@
         :id="item.id"
         :markerId="item.id"
         :show="item.show"
+        :comment="item.cell_id + '<br/>' + item.short_cell_id"
         marker-type="placemark"
         :coords="[item.lat, item.lon]"
         :icon="{ content: item.dbm }"
